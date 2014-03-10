5217abbd8f4742679a814d7104d4f3ee60a64e45
Steven Feltner <steven.feltner@gmail.com>
2014-03-10 08:42:29 -0700

    Added new files, updated backrc and rpmmacros and updated the makesymlinks script to include the new files.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim rpmmacros"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim rpmmacros git-completion.bash gitk screenrc"    # list of files/folders to symlink in homedir
 #files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
 
 ##########
