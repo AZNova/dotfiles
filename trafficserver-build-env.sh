ea3998d8442eb1e1b7c9c1a68c2fcdd5df136321
Steven Feltner <steven.feltner@gmail.com>
2018-05-16 22:09:09 -0400

    yum install build dependencies

--- trafficserver-build-env.sh
+++ trafficserver-build-env.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-sudo yum install -y centos-release-scl
-sudo yum-config-manager --enable rhel-server-rhscl-7-rpms
+#sudo yum install -y centos-release-scl
+#sudo yum-config-manager --enable rhel-server-rhscl-7-rpms
 #sudo yum-config-manager --enable epel
-sudo yum -y install scl-utils
+#sudo yum -y install scl-utils
 
-sudo yum install -y devtoolset-7
+#sudo yum install -y devtoolset-7
 
 # Optionally, install ASAN
 sudo yum install -y devtoolset-7-libasan-devel.x86_64
+
+sudo yum install -y gcc gcc-c++ pkgconfig pcre-devel tcl-devel expat-devel openssl-devel
+sudo yum install -y libcap libcap-devel hwloc hwloc-devel ncurses-devel libcurl-devel
+sudo yum install -y libunwind libunwind-devel
+sudo yum install -y autoconf automake libtool
+gcc -v
 
 mkdir -p ~/projects/
 cd ~/projects
